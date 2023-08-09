5d08931724273fa188254d6f49904621e7b27a6e
Iamneo <techadmin@iamneo.ai>
2023-08-09 11:15:57 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,9 +2,12 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+@SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.examly.model"}) 
 public class SpringappApplication {
 
 	public static void main(String[] args) {
