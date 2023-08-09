0a3e79dd29b5d06a67a566de0d628664f4f4c053
Iamneo <techadmin@iamneo.ai>
2023-08-09 05:40:13 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/TaskController.java
@@ -8,13 +8,62 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class TaskController {
 	@Autowired
 	private TaskService taskService;
+    private TaskRepository taskRepo;
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 
+    @RequestMapping("/saveTask")
+    public String saveTask(Task task) {
+        taskRepo.save(task);
+        return "index";
+    }
+
+    @RequestMapping(value = "/allTask", method = RequestMethod.GET)
+    public ModelAndView findAllUsers(ModelAndView mav) {
+        List<Task> tasks = taskRepo.findAll();
+        mav.addObject("tasks", tasks);
+        mav.setViewName("getData");
+        return mav;
+    }
+
+    @RequestMapping("/getTask")
+    public ModelAndView getuser(@RequestParam int id) {
+
+        ModelAndView mav = new ModelAndView("showUser");
+        User user = userdao.findById(id).orElse(new User());
+        mav.addObject(user);
+        return mav;
+    }
+
+    @RequestMapping("/changeStatus")
+    public ModelAndView updateuser(User user) {
+
+        ModelAndView mav = new ModelAndView("updateUser");
+        user = userdao.findById(user.getTaskId()).orElse(new User());
+        userdao.deleteById(user.getTaskId());
+        mav.addObject(user);
+        return mav;
+    }
+
+    @RequestMapping("/deleteTask")
+    public ModelAndView deleteuser(@RequestParam int id) {
+
+        ModelAndView mav = new ModelAndView("deleteUser");
+        User user = userdao.findById(id).orElse(new User());
+        userdao.deleteById(id);
+        mav.addObject(user);
+        return mav;
+    }
+
+	/*
 	@RequestMapping("/alltasks")
 	public List<Task> getAllSubjects()
 	{
@@ -48,7 +97,7 @@ public class TaskController {
 	
 	
 	
-	
+	*/
 	
 	
 }
